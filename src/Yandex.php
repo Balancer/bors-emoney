2e9c2cf9facdf66339ab3339124052d07c52a045
Balancer <balancer@balancer.ru>
2016-03-24 19:29:09 +0300

    More info.

--- src/Yandex.php
+++ src/Yandex.php
@@ -6,5 +6,14 @@ class Yandex extends \bors_object
 {
 	var $title = 'Яндекс.Деньги';
 	var $deposit_fees = '0';
-	var $withdrawal_fees = '0.03*amount';
+
+	var $withdrawal_with_fees = 'amount*0.97';
+	var $withdrawal_with_fees_back = 'amount/0.97';
+	var $withdrawal_fees_info = 'Яндекс.Деньги взымают 3% от стоимости выводимых средств';
+
+	var $deposit_with_fees = 'amount';
+	var $deposit_with_fees_back = 'amount';
+	var $deposit_fees_info = 'Яндекс.Деньги не взымают комиссии при принятии средств';
+
+	var $currency_short = 'RUB';
 }
